07e2a93af69a5d5f3d095bef671af1c1688a17a0
primer03 <Panupong.person@gmail.com>
2023-03-08 08:50:42 +0700

    ok

--- js/observer.js
+++ js/observer.js
@@ -1,6 +1,9 @@
 const targetElement = document.querySelectorAll('.target-element');
 const startx = document.querySelectorAll('.startx');
 const erabox = document.querySelectorAll('.erabox');
+const nameeraX = document.querySelectorAll('.nameeraX');
+var bocfera = document.querySelector('.bocfera')
+var imgfirstX = document.querySelectorAll('.imgfirstX')
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -37,6 +40,57 @@ const oberabox = new IntersectionObserver(entries => {
 }, {
     threshold: 0
 });
+
+const scalename = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log("OK");
+            entry.target.classList.add('nameerashow');
+        } else {
+            entry.target.classList.remove('nameerashow');
+        }
+    });
+}, {
+    threshold: 0
+});
+
+const opcity = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log("OK");
+            entry.target.classList.add('opacityshow');
+        } else {
+            entry.target.classList.remove('opacityshow');
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+const opcitynamefi = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('boxheadfshow');
+        } else {
+            entry.target.classList.remove('boxheadfshow');
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+const rorateX = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('imgfirstXshow');
+        } else {
+            entry.target.classList.remove('imgfirstXshow');
+        }
+    });
+}, {
+    threshold: 0.3
+});
+
 targetElement.forEach(e => {
     observer.observe(e);
 })
@@ -48,3 +102,14 @@ erabox.forEach(era => {
 startx.forEach(e => {
     observer1.observe(e);
 })
+
+nameeraX.forEach(e => {
+    scalename.observe(e);
+})
+
+imgfirstX.forEach(e => {
+    rorateX.observe(e);
+})
+
+opcity.observe(document.querySelector('.contentstart'))
+opcitynamefi.observe(bocfera)
